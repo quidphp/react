8828a5ae6a0f70084c837c29b190617d3148ffd5
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-23 09:51:22 -0400

    add redux

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,10 @@ const mix = require('laravel-mix');
 const path = require('path');
 require('laravel-mix-polyfill');
 
-mix.js('react/index.js', 'public/js/app.js');
+mix.js('app/index.js', 'public/js/app.js');
 mix.react({ extractStyles: 'public/css/app.css'});
 mix.extract(['react','react-dom','quidphp-navigation','quidphp-browser','quidphp-javascript'], 'public/js/app-vendor.js');
-mix.alias({ '@': path.join(__dirname, 'react') });
+mix.alias({ '@': path.join(__dirname, 'app') });
 
 if(mix.inProduction())
 mix.polyfill({ enabled: true, useBuiltIns: false });
@@ -14,5 +14,5 @@ else
 {
     mix.sourceMaps();
     const hmrOptions = { host: 'localhost', https: true, port: '8079' };
-    mix.options({ hmrOptions, resourceRoot: 'https://localhost:8079', });
+    mix.options({ hmrOptions });
 }
